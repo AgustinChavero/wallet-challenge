331b7a0a45d41d4fc9db4888d2deec71a092cfca
AgustinChavero <agustindanielchavero@gmail.com>
2025-11-25 17:52:21 -0300

    feature - controllers

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Client\RegisterClientRequest;
+
+class ClientController extends Controller
+{
+    private function soap()
+    {
+        $soapUrl = 'http://nginx/soap';
+
+        return new \SoapClient(null, [
+            'location' => $soapUrl,
+            'uri'      => $soapUrl,
+            'trace'    => 1,
+            'exceptions' => true
+        ]);
+    }
+
+    public function register(RegisterClientRequest $request)
+    {
+        try {
+            $response = $this->soap()->registerClient(
+                $request->document,
+                $request->names,
+                $request->email,
+                $request->phone
+            );
+
+            $data = json_decode($response->data, true);
+
+            return response()->json([
+                'success' => $response->success,
+                'cod_error' => $response->cod_error,
+                'message' => $response->message_error,
+                'data' => $data
+            ], $response->success ? 200 : 400);
+        } catch (\SoapFault $e) {
+            return response()->json([
+                'success' => false,
+                'cod_error' => '10',
+                'message_error' => 'SOAP Error: ' . $e->getMessage(),
+                'data' => null
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'cod_error' => '10',
+                'message_error' => $e->getMessage(),
+                'data' => null
+            ], 500);
+        }
+    }
+}
---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Wallet\BalanceWalletRequest;
+use App\Http\Requests\Wallet\RechargeWalletRequest;
+
+class WalletController extends Controller
+{
+    private function soap()
+    {
+        $soapUrl = 'http://nginx/soap';
+
+        return new \SoapClient(null, [
+            'location' => $soapUrl,
+            'uri'      => $soapUrl,
+            'trace'    => 1,
+            'exceptions' => true
+        ]);
+    }
+
+    public function recharge(RechargeWalletRequest $request)
+    {
+        try {
+            $response = $this->soap()->rechargeWallet(
+                $request->document,
+                $request->phone,
+                $request->amount
+            );
+
+            $data = json_decode($response->data, true);
+
+            return response()->json([
+                'success' => true,
+                'cod_error' => '00',
+                'message' => $response->message_error,
+                'data' => $data
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'cod_error' => '10',
+                'message_error' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    public function balance(BalanceWalletRequest $request)
+    {
+        try {
+            $response = $this->soap()->checkBalance(
+                $request->document,
+                $request->phone
+            );
+
+            $data = json_decode($response->data, true);
+
+            return response()->json([
+                'success' => true,
+                'cod_error' => '00',
+                'message' => $response->message_error,
+                'data' => $data
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'cod_error' => '10',
+                'message_error' => $e->getMessage()
+            ], 400);
+        }
+    }
+}
