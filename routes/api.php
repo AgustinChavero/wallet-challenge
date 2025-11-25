8434cb3f1cd94aa63dc14805c5a6fcc57f132aae
AgustinChavero <agustindanielchavero@gmail.com>
2025-11-25 10:47:41 -0300

    feature - rest & soap routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\WalletController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('wallet')->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('/register', [WalletController::class, 'register']);
+
+    Route::post('/recharge', [WalletController::class, 'recharge']);
+
+    Route::post('/pay', [WalletController::class, 'pay']);
+
+    Route::post('/confirm', [WalletController::class, 'confirm']);
+
+    Route::post('/balance', [WalletController::class, 'balance']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Soap\Services\WalletSoapService;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::any('/soap', function () {
 
-Route::get('/', function () {
-    return view('welcome');
+    $server = new SoapServer(null, [
+        'uri' => url('/soap'),
+    ]);
+
+    $server->setClass(WalletSoapService::class);
+
+    $server->handle();
 });
